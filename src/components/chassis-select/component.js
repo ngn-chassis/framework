e6cd26bcda43926c2611b13e7ecd2f0e211f19e6
Graham Butler <graham@grahambutler.com>
2017-11-07 23:13:18 -0800

    remove unused variables from chassis-select spec sheet

--- src/components/chassis-select/component.js
+++ src/components/chassis-select/component.js
@@ -1,7 +1,7 @@
 class ChassisSelectComponent {
   constructor (chassis) {
     this.chassis = chassis
-    this.resetType = 'none'
+    this.resetType = 'inline-block'
   }
 
   get variables () {
@@ -15,10 +15,7 @@ class ChassisSelectComponent {
     return {
       'icon-offset': `translateX(-${(typography.calculateInlinePaddingX(lineHeightMultiplier) / 2) - utils.units.toEms(fontSize / (settings.typography.scaleRatio * 10), fontSize)}em)`,
       'pill-padding-x': `${settings.typography.scaleRatio}em`,
-      'pill-border-radius': `${lineHeightMultiplier}em`,
-      'multi-line-padding-y': `${padding}em`,
-			'multi-line-line-height': `${lineHeightMultiplier}`,
-			'multi-line-white-space': 'normal'
+      'pill-border-radius': `${lineHeightMultiplier}em`
     }
   }
 }
