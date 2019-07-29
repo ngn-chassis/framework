396e0b86521a13cfd2f079ad4fe8036f8e55615f
Graham Butler <graham@grahambutler.com>
2019-07-28 22:23:38 -0700

    add author-icon component support

---
+++ components/author-icon/component.js
@@ -0,0 +1,21 @@
+module.exports = class {
+	constructor (chassis) {
+		Object.defineProperties(this, {
+			chassis: NGN.privateconst(chassis)
+		})
+
+		this.resetType = 'inline-block'
+	}
+
+	get variables () {
+		let { settings, utils } = this.chassis
+		let { fontSize, lineHeight } = settings.typography.ranges.first.typography.root
+
+		let lineHeightInEms = utils.unit.pxToEm(lineHeight, fontSize)
+
+		return {
+			'width': 'auto',
+			'height': `${lineHeightInEms}em`
+		}
+	}
+}
